321ff2dd072f0271f28f1078931af31d4c2dd32b
vistart <i@vistart.me>
2021-05-05 19:13:58 +0800

    增加设备ID。

--- examples/benchmark/pvcnn/PVCNNBenckmark.cpp
+++ examples/benchmark/pvcnn/PVCNNBenckmark.cpp
@@ -20,7 +20,7 @@
 #ifndef CUDA_DURATION_EVENT
 #define CUDA_DURATION_EVENT 0
 #endif
-void PVCNNBenchmark(const at::Tensor& c1, const at::Tensor& c2, unsigned int depth = 12)
+void PVCNNBenchmark(const at::Tensor& c1, const at::Tensor& c2, unsigned int depth = 12, const unsigned int device_id = 0)
 {
 #if CUDA_DURATION_EVENT
     cudaEvent_t start, stop;
@@ -42,7 +42,7 @@ void PVCNNBenchmark(const at::Tensor& c1, const at::Tensor& c2, unsigned int dep
     cudaDeviceSynchronize();
     std::chrono::steady_clock::time_point time_stop = std::chrono::steady_clock::now();
     const std::chrono::duration<double> duration = std::chrono::duration_cast<std::chrono::duration<double>>(time_stop - time_start1);
-    std::cout << "1 queried." << " Elapsed: " << duration.count() << " s" << std::endl;
+    std::cout << "1 queried." << " Elapsed: " << std::setw(12) << duration.count() << " s" << std::endl;
     time_start1 = std::chrono::steady_clock::now();
 #endif
 }
@@ -52,6 +52,7 @@ int main(int argc, char* argv[])
     unsigned int num_pointers = 10000000;
     unsigned int batch = 250;
     unsigned int mode = 3;
+    unsigned int device_id = 0;
     if (argc > 1)
     {
         mode = atoi(argv[1]);
@@ -68,14 +69,19 @@ int main(int argc, char* argv[])
         std::cout << "The total of pointers should not be less than 100000, and the batch should not be less than 4." << std::endl;
         return 0;
     }
+	if (argc > 4)
+	{
+        device_id = atoi(argv[4]);
+	}
     torch::manual_seed(1);
 	for (int i = 0; i < batch; i++)
 	{
+	    std::cout << "Batch (" << std::setw(8) << i << "): ";
         const at::Tensor coords = at::rand({ batch, 3, num_pointers / batch });
         const auto& c1 = torch::clamp(torch::round(coords * pow(2, 12)), 0, pow(2, 12) - 1);
         const at::Tensor coords_to_be_compared = at::rand({ batch, 3, num_pointers / batch });
         const auto& c2 = torch::clamp(torch::round(coords_to_be_compared * pow(2, 12)), 0, pow(2, 12) - 1);
-        PVCNNBenchmark(c1, c2);
+        PVCNNBenchmark(c1, c2, device_id);
 	}
 	return 0;
 }
